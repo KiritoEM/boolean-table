e2b57345ae4fca2cf276fc2c93592077c16c1952
KiritoEM <johankirito64@gmail.com>
2025-02-27 10:37:16 +0300

    add tokenizer

--- src/tokenizer.ts
+++ src/tokenizer.ts
@@ -1,5 +1,5 @@
 import type { ITokenizer } from "./types";
-import { NON_SYMBOL, WHITESPACES } from "./utils/rules";
+import { NON_SYMBOL, SPACES, WHITESPACES, WORD } from "./utils/rules";
 
 class Tokenizer implements ITokenizer {
     tokenizeInput(str: string): string[] {
@@ -8,10 +8,10 @@ class Tokenizer implements ITokenizer {
         }
 
         let i = 0;
-        let words = str.split(/\b/);
+        let words = str.split(WORD);
 
         while (i < words.length) {
-            words[i] = words[i].replace(/\s/g, "");
+            words[i] = words[i].replace(SPACES, "");
 
             if (!words[i].length) {
                 words.splice(i, 1); //remove empty strings
@@ -19,8 +19,10 @@ class Tokenizer implements ITokenizer {
             else if (!NON_SYMBOL.test(words[i])) {
                 let arr = [];
 
-                for (let j = 0; j < words[i].length; j++) {
+                let j = 0;
+                while (j < words[i].length) {
                     arr.push(words[i][j]);
+                    j++;
                 }
 
                 words.splice(i, 1, ...arr); //replace current symbol token with new arr tokens
--- src/utils/rules.ts
+++ src/utils/rules.ts
@@ -1,2 +1,4 @@
 export const WHITESPACES = /^\s*$/;
 export const NON_SYMBOL = /[a-zA-Z]\w*/;
+export const WORD = /\b/;
+export const SPACES = /\s/g;
